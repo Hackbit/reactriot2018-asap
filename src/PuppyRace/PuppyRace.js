85e9c73cd70de9eb26f50ebcdb77afba0f99aedf
Youngrok Kim <rok0810@gmail.com>
2018-09-16 13:17:19 +0900

    Add constants and context

--- src/PuppyRace/PuppyRace.js
+++ src/PuppyRace/PuppyRace.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { RaceTrack } from './RaceTrack';
 import { Settings } from './Settings';
+import { PuppyRaceContext } from './PuppyRaceContext';
+import { GAME_STATUS } from './constants';
 
 export class PuppyRace extends React.Component {
+  state = {
+    animals: [],
+    status: GAME_STATUS.READY
+  };
+
   render() {
     return (
-      <div>
-        <RaceTrack />
-        <Settings />
-      </div>
+      <PuppyRaceContext.Provider value={{ state, actions }}>
+        <div>
+          <RaceTrack />
+          <Settings />
+        </div>
+      </PuppyRaceContext.Provider>
     );
   }
 }
---
+++ src/PuppyRace/constants.js
@@ -0,0 +1,17 @@
+export const GAME_STATUS = {
+  READY: 0,
+  START: 1,
+  PAUSE: 2,
+  END: 3
+};
+
+export const ANIMAL_TYPES = {
+  DOG: 'dog',
+  CAT: 'cat'
+};
+
+export const ANIMAL_STATUS = {
+  STANDING: 0,
+  RUNNING: 1,
+  LOSS: 2
+};
