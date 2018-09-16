b205da723ac32731393a72816dbd7ca7bbf49d13
Youngrok Kim <rok0810@gmail.com>
2018-09-16 13:05:59 +0900

    Seperate PuppyRace component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { PuppyRace } from './PuppyRace';
+import logo from 'logo.svg';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div class="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">React Puppy Race</h1>
+    </header>
+    <PuppyRace />
+  </div>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
