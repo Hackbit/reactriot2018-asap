fe90ef21ae52285aa3f6693334bc68028f707be8
ikanny <dev.ikanny@gmail.com>
2018-09-16 13:29:54 +0900

    Add dog and cat sprites

---
+++ src/PuppyRace/Animal/Animal.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class Animal extends React.Component {
+  render() {
+    return <div>Animal</div>;
+  }
+}
--- src/PuppyRace/Puppy/Puppy.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export class Puppy extends React.Component {
-  render() {
-    return <div>Puppy</div>;
-  }
-}
