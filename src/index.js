34e8e6d3e1846c10bbc9f5a2a057df8d33dddfe4
Youngrok Kim <rok0810@gmail.com>
2018-09-16 14:08:22 +0900

    Update root style

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { PuppyRace } from './PuppyRace';
-import logo from 'logo.svg';
 import './index.css';
 
 ReactDOM.render(
   <div class="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={require('./react-logo.svg')} className="App-logo" alt="logo" />
       <h1 className="App-title">React Puppy Race</h1>
     </header>
     <PuppyRace />
+    <footer className="App-footer">
+      <p>
+        Dog and cat image are from{' '}
+        <a href="https://dribbble.com/shots/2157574-BFF-s">BFF's</a> by{' '}
+        <a href="https://dribbble.com/madebyradio">R A D I O</a>
+      </p>
+    </footer>
   </div>,
   document.getElementById('root')
 );
