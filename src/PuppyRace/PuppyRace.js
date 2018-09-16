54a3c227911201d1e1fdf8c90a9796ecd1af56df
Youngrok Kim <rok0810@gmail.com>
2018-09-16 13:54:16 +0900

    Improve start & reset button style

--- src/PuppyRace/PuppyRace.js
+++ src/PuppyRace/PuppyRace.js
@@ -103,22 +103,26 @@ export class PuppyRace extends React.Component {
         <div>
           <RaceTrack />
           <Settings />
-          <div>
+          <div className="ui fluid">
             <button
+              className="ui blue button"
               onClick={() =>
                 !isPlaying ? actions.startGame() : actions.pauseGame()
               }
               disabled={state.status === GAME_STATUS.END}
             >
+              <i className={`ui icon ${!isPlaying ? 'play' : 'pause'}`} />
               {!isPlaying ? 'START' : 'PAUSE'}
             </button>
             <button
+              className="ui green button"
               onClick={() => actions.resetGame()}
               disabled={
                 state.status === GAME_STATUS.START ||
                 state.status === GAME_STATUS.READY
               }
             >
+              <i className={`ui icon refresh`} />
               RESET
             </button>
           </div>
